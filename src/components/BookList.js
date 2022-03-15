dda97798c18e86cd24df5365d96d726621ff6ed5
Aduda-Boaz <adudaboaz@gmail.com>
2022-03-15 21:21:32 +0300

    fix linter errors

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-const percentage = 67;
-
 function BookList() {
   return (
     <div className="List-container">
